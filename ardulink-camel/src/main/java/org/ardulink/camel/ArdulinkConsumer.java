70218b3a149fddc9cb489a55f9030c020769929e
Luciano Zu <luciano.zu@gmail.com>
2016-06-06 17:29:52 +0200

    Comsumer implementation

--- ardulink-camel/src/main/java/org/ardulink/camel/ArdulinkConsumer.java
+++ ardulink-camel/src/main/java/org/ardulink/camel/ArdulinkConsumer.java
@@ -1,5 +1,8 @@
 package org.ardulink.camel;
 
+import org.apache.camel.Exchange;
+import org.apache.camel.ExchangePattern;
+import org.apache.camel.Message;
 import org.apache.camel.Processor;
 import org.apache.camel.impl.DefaultConsumer;
 import org.ardulink.core.messages.events.api.InMessageEvent;
@@ -13,9 +16,35 @@ public class ArdulinkConsumer extends DefaultConsumer implements InMessageListen
 
 	@Override
 	public void inMessageReceived(InMessageEvent e) {
-		
-		// TODO chiamare i metodi del default consumer studiare.... SONO QUI.
-		
+
+        Exchange exchange = getEndpoint().createExchange(ExchangePattern.InOnly);
+        Message message = exchange.getIn();
+
+        message.setBody(e.getInMessage());
+		try {
+			getProcessor().process(exchange);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			getExceptionHandler().handleException("Failed to process notification", ex);
+		}
 	}
+
+	@Override
+	protected void doStart() throws Exception {
+		ArdulinkEndpoint ardulinkEndpoint = (ArdulinkEndpoint)getEndpoint();
+		
+		ardulinkEndpoint.getLinkMessageAdapter().addInMessageListener(this);
+		super.doStart();
+	}
+	
+	@Override
+	protected void doStop() throws Exception {
+		ArdulinkEndpoint ardulinkEndpoint = (ArdulinkEndpoint)getEndpoint();
+		
+		ardulinkEndpoint.getLinkMessageAdapter().removeInMessageListener(this);
+		super.doStop();
+	}
+
+	
 
 }
